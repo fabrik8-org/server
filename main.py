9e3114deec03e46bb8210be698055424dad4e1e3
Dulaj Prabasha <shad3s.of.m3@gmail.com>
2023-05-06 20:37:50 +0530

    add extra endpoint

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import io
+import os
 from PIL import Image
 import cv2
 import numpy as np
@@ -11,13 +14,10 @@ import requests
 
 from model_funcs import draw_bounding_boxes
 
-#Neptune for monitoring
+# Neptune for monitoring
 import neptune
 from dotenv import load_dotenv
 load_dotenv()
-import os
-import io
-import time
 
 app = Flask(__name__)
 CORS(app)
@@ -51,6 +51,35 @@ def health_check():
     return jsonify({"message": "Healthy"}), 200
 
 
+@app.route('/adjust', methods=['POST'])
+def adjust_image():
+    # Get the base64 string from the request
+    data = request.get_json()
+    base64_string = data.get('image')
+
+    # Decode the base64 string into bytes
+    image_bytes = base64.b64decode(base64_string)
+
+    # Open the image using PIL
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # Resize the image to 512x512
+    resized_image = image.resize((512, 512))
+
+    # Create an in-memory buffer to store the resized image
+    output_buffer = io.BytesIO()
+
+    # Save the resized image to the buffer in JPEG format
+    resized_image.save(output_buffer, format='PNG')
+
+    # Get the base64 string of the resized image
+    resized_base64_string = base64.b64encode(
+        output_buffer.getvalue()).decode('utf-8')
+
+    # Return the resized base64 string
+    return jsonify({"image": resized_base64_string}), 200
+
+
 @app.route('/predict', methods=['POST'])
 def predict_defect():
     run = init_monitoring()
@@ -70,13 +99,14 @@ def predict_defect():
     run['boundingBoxes/bounding_boxes'] = str(output[0]['boxes'])
     run['boundingBoxes/labels'] = str(output[0]['labels'])
     run['boundingBoxes/scores'] = str(output[0]['scores'])
-    #This is the image with only the bounding boxes of the defects
+    # This is the image with only the bounding boxes of the defects
     image_base64 = base64.b64encode(result['image']).decode('utf-8')
     img_output = base64.b64decode(image_base64)
     image_output = Image.open(io.BytesIO(img_output))
     run['image_output'].upload(image_output)
-    #This is for the image with all the bounding boxes
-    image_base64_all = base64.b64encode(result['image_all_boxes']).decode('utf-8')
+    # This is for the image with all the bounding boxes
+    image_base64_all = base64.b64encode(
+        result['image_all_boxes']).decode('utf-8')
     img_output_all = base64.b64decode(image_base64_all)
     image_output_all = Image.open(io.BytesIO(img_output_all))
     run['image_output_all_bounding_boxes'].upload(image_output_all)
